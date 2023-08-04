8a5ba6bd9ef8cd6ed1dc3126db1a4268029ccc1f
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-08-04 17:43:29 +0700

    done APIs

--- src/main/java/caption/java/movies/Movie.java
+++ src/main/java/caption/java/movies/Movie.java
@@ -25,5 +25,5 @@ public class Movie {
      private List<String> genres;
      private List<String> backdrops;
      @DocumentReference
-     private List<Review> reviewIDs;
+     private List<Review> reviewIds;
 }
